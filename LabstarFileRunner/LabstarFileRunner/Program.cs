1544a315dcef35f51e82fddfb3cf08d52c733dcd
Петухов Антон Алексеевич <anton.petuhov@fccho-moscow.ru>
2024-01-10 16:53:48 +0300

    Labstar drivers. filerunner + console

---
+++ LabstarFileRunner/LabstarFileRunner/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LabstarFileRunner
+{
+    class Program
+    {
+        #region settings
+        public static string user = "PSMExchangeUser"; // логин для базы обмена файлами и для базы CGM Analytix
+        public static string password = "PSM_123456";  // пароль для базы обмена файлами и для базы CGM Analytix
+
+        public static bool ServiceIsActive;            // флаг для запуска и остановки потока
+
+        static object FileExportLogLocker = new object();    // локер для логов обмена
+        static object ServiceLogLocker = new object();       // локер для логов драйвера
+        #endregion
+
+        #region фунции логов
+
+        // Лог драйвера
+        static void ServiceLog(string Message)
+        {
+            lock (ServiceLogLocker)
+            {
+                try
+                {
+                    string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\Service";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    string filename = path + "\\ServiceThread_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+                    if (!System.IO.File.Exists(filename))
+                    {
+                        using (StreamWriter sw = System.IO.File.CreateText(filename))
+                        {
+                            sw.WriteLine(DateTime.Now + ": " + Message);
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter sw = System.IO.File.AppendText(filename))
+                        {
+                            sw.WriteLine(DateTime.Now + ": " + Message);
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        // лог записи файла в FNC2
+        static void FileExportLog(string Message)
+        {
+            lock (FileExportLogLocker)
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\FileExport";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string filename = path + "\\ExportThread_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+                if (!System.IO.File.Exists(filename))
+                {
+                    using (StreamWriter sw = System.IO.File.CreateText(filename))
+                    {
+                        sw.WriteLine(DateTime.Now + ": " + Message);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = System.IO.File.AppendText(filename))
+                    {
+                        sw.WriteLine(DateTime.Now + ": " + Message);
+                    }
+                }
+            }
+        }
+
+        // проверка старых логов
+        public void CheckOldLog()
+        {
+            while (ServiceIsActive)
+            {
+                try
+                {
+                    ServiceLog("Проверка логов");
+                    //сегодня минус 28 дней
+                    //все что старше удаляем
+                    DateTime now = DateTime.Now.AddDays(-28);
+                    string LogsDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\Service";
+
+                    if (Directory.Exists(LogsDirectory))
+                    {
+                        string[] files = Directory.GetFiles(LogsDirectory);
+                        foreach (string s in files)
+                        {
+                            DateTime FileDate = File.GetCreationTime(s);
+                            int comparison = FileDate.CompareTo(now);
+                            if (comparison == -1)
+                            {
+                                File.Delete(s);
+                            }
+
+                        }
+                    }
+
+                }
+                catch (Exception LogError)
+                {
+                    ServiceLog("Ошибка: " + LogError);
+                }
+                Thread.Sleep(4320000);
+            }
+
+        }
+
+        #endregion
+
+        #region функция отправки файлов в базу FNC (cgm-data01), таблица FileExchange
+        public static void WorkingWithOutFiles(string file)
+        {
+            string ServiceId = ConfigurationManager.AppSettings["ServiceCode"];
+            string OutFolder = ConfigurationManager.AppSettings["FolderOut"];
+            // папка для ошибок
+            string ErrorPath = OutFolder + @"\errors";
+            if (!Directory.Exists(ErrorPath))
+            {
+                Directory.CreateDirectory(ErrorPath);
+            }
+
+            // обрезаем только имя текущего файла
+            string FileName = file.Substring(OutFolder.Length + 1);
+
+            string CGMConnectionString = ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString;
+            CGMConnectionString = String.Concat(CGMConnectionString, $"User Id = {user}; Password = {password}");
+
+            try
+            {
+                using (SqlConnection CGMconnection = new SqlConnection(CGMConnectionString))
+                {
+                    CGMconnection.Open();
+
+                    //SqlCommand SqlInsertCommand = new SqlCommand();
+                    SqlCommand SqlInsertCommand = CGMconnection.CreateCommand();
+                    SqlInsertCommand.CommandText = "INSERT INTO FileExchange VALUES (@filename, @file, @changeuser, @changedate, @serviceid, 0 )";
+
+                    // создаем параметры для инсерта
+                    SqlParameter filenameParam = new SqlParameter("@filename", SqlDbType.NVarChar, 200);
+                    SqlParameter fileParam = new SqlParameter("@file", SqlDbType.Image, 1000000);
+                    SqlParameter changeuserParam = new SqlParameter("@changeuser", SqlDbType.NVarChar, 50);
+                    SqlParameter changedateParam = new SqlParameter("@changedate", SqlDbType.NVarChar, 50);
+                    SqlParameter serviceidParam = new SqlParameter("@serviceid", SqlDbType.NVarChar, 50);
+
+                    // добавляем параметры к команде
+                    SqlInsertCommand.Parameters.Add(filenameParam);
+                    SqlInsertCommand.Parameters.Add(fileParam);
+                    SqlInsertCommand.Parameters.Add(changeuserParam);
+                    SqlInsertCommand.Parameters.Add(changedateParam);
+                    SqlInsertCommand.Parameters.Add(serviceidParam);
+
+                    // массив для хранения бинарных данных файла
+                    byte[] imageData;
+                    using (System.IO.FileStream fs = new System.IO.FileStream(file, FileMode.Open))
+                    {
+                        imageData = new byte[fs.Length];
+                        fs.Read(imageData, 0, imageData.Length);
+                    }
+
+                    string NowString = DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day + " " + DateTime.Now.ToShortTimeString() + ":" + DateTime.Now.Second + "." + DateTime.Now.Millisecond;
+
+                    //string FileName = file.Substring(OutFolder.Length + 1);
+
+                    // передаем данные в команду через параметры
+                    SqlInsertCommand.Parameters["@filename"].Value = FileName;
+                    SqlInsertCommand.Parameters["@file"].Value = imageData;
+                    SqlInsertCommand.Parameters["@changeuser"].Value = ServiceId;
+                    SqlInsertCommand.Parameters["@changedate"].Value = NowString;
+                    SqlInsertCommand.Parameters["@serviceid"].Value = ServiceId;
+
+                    SqlInsertCommand.ExecuteNonQuery();
+                }
+
+                File.Delete(file);
+                FileExportLog($"Файл {file} обработан и записан в базу FNC2.");
+                FileExportLog("");
+            }
+            catch(Exception ex)
+            {
+                FileExportLog(ex.Message);
+                // помещение файла в папку с ошибками
+                if (File.Exists(ErrorPath + @"\" + FileName))
+                {
+                    File.Delete(ErrorPath + @"\" + FileName);
+                }
+                File.Move(file, ErrorPath + @"\" + FileName);
+
+                FileExportLog("Ошибка обработки файла. Файл перемещен в папку Error");
+                FileExportLog("");
+            }
+        }
+        #endregion
+
+        #region поток мониторинга и отправки файлов с результатами в базу FNC2, таблица FileExchangeIn
+        // поток мониторинга и отправки файлов с результатами в базу FNC2, таблица FileExchangeIn
+        public static void GetOut()
+        {
+            int ServerCount = 0; // счетчик
+
+            while (ServiceIsActive)
+            {
+                ServerCount++;
+                if (ServerCount == 100)
+                {
+                    ServerCount = 0;
+                    ServiceLog("waiting...");
+                }
+
+                try
+                {
+                    string OutFolder = ConfigurationManager.AppSettings["FolderOut"];
+
+                    if (Directory.Exists(OutFolder))
+                    {
+                        string[] FileFormat = ConfigurationManager.AppSettings["FileType"].Split(';');
+                        foreach (string format in FileFormat)
+                        {
+                            string[] Files = Directory.GetFiles(OutFolder, format);
+                            foreach (string file in Files)
+                            {
+                                FileExportLog($"Файл: {file}");
+                                WorkingWithOutFiles(file);
+                            }
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    FileExportLog(ex.Message);
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            ServiceIsActive = true;
+
+            //поток для отправки файлов в базу
+            Thread GetOutThread = new Thread(new ThreadStart(GetOut));
+            GetOutThread.Name = "GetOutFiles Thread";
+            GetOutThread.Start();
+            ServiceLog("Service is started");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabstarFileRunnerService/LabstarFileRunnerService/Installer1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace LabstarFileRunnerService
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+        public Installer1()
+        {
+            InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "Labstar FileRunner";
+            serviceInstaller.Description = "Copying Labstar .xml files to FNC db";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
